80aa35318ebdfc919a675f134d373363b0c6ed30
maiosama <mai.osama1001@gmail.com>
2021-04-15 19:40:10 +0200

    fix the passed data to the SearchPage Component

--- src/App.js
+++ src/App.js
@@ -10,49 +10,51 @@ import {Route, Link} from 'react-router-dom'
 class BooksApp extends React.Component {
     state ={
         books:[],
-        showSearchPage: false
     };
     // call the BookAPI backend to have a book array
     componentDidMount(){
         BooksAPI.getAll()
         .then((books)=>{
-            this.setState(({books}));
-            console.log ("apps page books: ", books)
+            this.setState({books});
         })
+        .then(()=> {console.log ("apps page books: ", this.state.books)
+    })
     };
     // send a request to the BookAPI to update the book shelf in the back end
-    shelfChanger= (book,changedShelf)=>{
-        BooksAPI.update(book,changedShelf);
-        book.shelf=changedShelf;
+    shelfChanger= (book,updatedShelf)=>{
+        BooksAPI.update(book,updatedShelf)
+        .then(response=>{book.shelf=updatedShelf;});
+        
         const books=[...this.state.books].filter((each)=> each.id!==book.id).concat(book);
-        this.setState(({books}));
+        this.setState({books});
     };
+    
 
     render() {
         return (
             <div className="app">
               {/* the main book shelf page */}
-                <Route path ='/search' 
-                render={()=>(<SearchPage 
+               <Route path= '/search'>
+               <SearchPage 
                 books ={this.state.books}
                 shelfChanger={this.shelfChanger}/>
-                )}/>
-
+                </Route>
+                
               {/* search page */}
-                <Route exact path ='/' 
-                render={()=>(
+                <Route exact path='/'>
                 <BookShelves 
-                books ={this.searchResult}
+                books ={this.state.books}
                 shelfChanger={this.shelfChanger}/>
-                )}/>
+              </Route>
 
                 {/* Add book button  */}
                 <div className="open-search">
                    <Link to='search'> 
-                        <button onClick={() => this.setState({showSearchPage: true})}>
+                        <button>
                          Add a book
                         </button>
                     </Link>
+                    
                 </div>   
             
           
